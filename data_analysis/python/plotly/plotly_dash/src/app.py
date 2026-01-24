e4e7ec2f4a135b77536e056752f343b0ffc2393f
Leverson Batista <leverbat06@gmail.com>
2026-01-24 14:44:10 -0300

    Projeto inicial: dashboard simples com Dash e Plotly

--- data_analysis/python/plotly/plotly_dash/src/app.py
+++ data_analysis/python/plotly/plotly_dash/src/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -6,27 +6,39 @@ app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_excel("../data/Vendas.xlsx")
 
 #criando o gráfico
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+opcoes = list(df['ID Loja'].unique())
+opcoes.append("Todas as Lojas")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Faturamento das Lojas'),
+    html.H2(children='Gráfico com o Faturamento de Todos os Produtos separados por Loja'),
 
     html.Div(children='''
-        Dash: A web application framework for your data.
+        Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
     '''),
-
+    dcc.Dropdown(opcoes, value='Todas as Lojas', id='lista_lojas'),
     dcc.Graph(
-        id='example-graph',
+        id='grafico_quantidade_vendas',
         figure=fig
     )
 ])
+
+@app.callback(
+    Output('grafico_quantidade_vendas', 'figure'),
+    Input('lista_lojas', 'value')
+)
+
+def update_output(value):
+    if value == 'Todas as Lojas':
+        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+    else:
+        tabela_filtrada = df.loc[df['ID Loja'] == value, :]
+        fig = px.bar(tabela_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
