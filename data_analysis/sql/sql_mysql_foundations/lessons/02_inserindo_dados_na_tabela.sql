20f4edf7ef5adaa8196fe8c636cf9d7480b22abb
Leverson Batista <leverbat06@gmail.com>
2026-01-29 13:48:20 -0300

    fix(sql): adiciona conteudo da aula de insercao de dados

---
+++ data_analysis/sql/sql_mysql_foundations/lessons/02_inserindo_dados_na_tabela.sql
@@ -0,0 +1,16 @@
+/* INSERT INTO pessoas 
+(id, nome, nascimento, sexo, peso, altura, nacionalidade) 
+VALUES
+(DEFAULT, 'Creuza', '1920-12-30', 'F', '50.2', '1.65', DEFAULT);
+
+ INSERT INTO pessoas VALUES 
+(DEFAULT, 'Adalgiza', '1930-11-2', 'F', '63.2', '1.75', 'Irlanda'); */
+
+INSERT INTO pessoas 
+(id, nome, nascimento, sexo, peso, altura, nacionalidade) 
+VALUES
+(DEFAULT, 'Ana', '1975-12-22', 'F', '52.3', '1.45', 'EUA'),
+(DEFAULT, 'Pedro', '2000-07-15', 'M', '67.2', '1.70', 'Brasil'),
+(DEFAULT, 'Maria', '1999-05-30', 'F', '75.9', '1.72', 'Portugal');
+
+SELECT * FROM pessoas;
